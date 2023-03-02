a20f0d5e698400ad9e0c57ce7484dde557951d62
Jean DeZonia <jspd1105@gmail.com>
2023-03-02 11:47:00 -0600

    adding autowiring, and more.

---
+++ learn-spring-framework-02/src/main/java/com/spring/example/learnspringframework/examples/c1/DataService.java
@@ -0,0 +1,9 @@
+package com.spring.example.learnspringframework.examples.c1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface DataService {
+
+    int[] retrieveData();
+}
