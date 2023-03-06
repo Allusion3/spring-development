940bc805eef8252a9ed6a468cefdf14df7c3a502
Jean DeZonia <jspd1105@gmail.com>
2023-03-06 15:27:09 -0600

    adding RestController

--- learn-spring-boot/src/main/java/com/dezonia/example/springboot/learnspringboot/CourseController.java
+++ learn-spring-boot/src/main/java/com/dezonia/example/springboot/learnspringboot/CourseController.java
@@ -13,7 +13,9 @@ public class CourseController {
     public List<Course> retrieveAllCourses(){
         return Arrays.asList(
                 new Course(1, "Learn AWS", "Jean DeZonia"),
-                new Course(2, "Learn DevOps", "Brittney DeZonia")
+                new Course(2, "Learn DevOps", "Brittney DeZonia"),
+                new Course(3, "Learn Google Cloud", "Ollie"),
+                new Course(4, "Learn Google", "Otis")
         );
     }
 }
