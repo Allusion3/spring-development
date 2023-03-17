d928154d46315f3dbfc6e21c2bfafc93dbcb52eb
Jean DeZonia <jspd1105@gmail.com>
2023-03-17 13:12:08 -0500

    updating look of pages

--- myfirstwebapp/src/main/java/com/springdevelopment/springboot/myfirstwebapp/todo/TodoService.java
+++ myfirstwebapp/src/main/java/com/springdevelopment/springboot/myfirstwebapp/todo/TodoService.java
@@ -3,17 +3,21 @@ package com.springdevelopment.springboot.myfirstwebapp.todo;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class TodoService {
 
-    private static List<Todo> todos;
+    private static List<Todo> todos = new ArrayList<>();
 
     static {
-        todos.add(new Todo(1, "jdezonia", "Learn AWS", LocalDate.now().plusMonths(5), false));
-        todos.add(new Todo(2, "jdezonia", "Learn GCLOUD", LocalDate.now().plusMonths(7), false));
-        todos.add(new Todo(3, "jdezonia", "Learn SPRING", LocalDate.now().plusMonths(12), false));
+        todos.add(new Todo(1, "jdezonia", "Learn AWS",
+                LocalDate.now().plusMonths(5), false));
+        todos.add(new Todo(2, "jdezonia", "Learn GCLOUD",
+                LocalDate.now().plusMonths(7), false));
+        todos.add(new Todo(3, "jdezonia", "Learn SPRING",
+                LocalDate.now().plusMonths(12), false));
 
     }
 
--- myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
+++ myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -1,10 +1,10 @@
 <html>
     <head>
-        <title>My Login Page</title>
+        <title>Welcome Page</title>
     </head>
         <body>
-        <div>Welcome to my page thanks for signing up!</div>
-        <div>Your Name: ${name}</div>
-        <div>Your Password: ${password}</div>
+        <div>Welcome ${name}</div>
+        <hr>
+        <div><a href="list-todos">Manager</a> your todos</div>
         </body>
 </html>
